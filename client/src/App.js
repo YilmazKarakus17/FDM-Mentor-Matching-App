3bccb6ea0da92bdb707584e8e6f18b9d32a042ac
YilmazKarakus <yilmazkarakus14@outlook.com>
2021-03-25 19:26:59 +0000

    creating folders for components

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,58 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import './App.css';
 import Axios from 'axios';
 
-function App() {
+export default class App extends React.Component{
+  constructor(props){
+    super(props)
+  }
 
-  return (
-    <div className="App">
-      Hello
-    </div>
-  );
+  myPostFunction(){
+    Axios.put('http://localhost:3001/api/update/mentee/mentor-id', {
+      mentorId: "fdm2",
+      fdmEmail: "fdm0007@fdm.co.uk"
+    }).then((response) => {
+      console.log(response)
+    });
+  }
+
+  render(){
+    //logic code
+    return (
+      <div id="app">
+        <button onClick={this.myPostFunction}>Post</button>
+      </div>
+    )
+  }
 }
 
-export default App;
+// Axios.post('http://localhost:3001/api/insert/mentee', {
+//   fdmEmail: 'fdm0007@fdm.co.uk',
+//   pwd: '12345',
+//   fname: 'jo-allan',
+//   lname: 'smith',
+//   desc: 'software engineer, good at testing',
+//   img: 'testImage.pic',
+//   email: 'joAllan@outlook.com',
+//   phone: '07958585145'
+// }).then((response) => {
+//   console.log(response)
+// });
 
+
+// let fdmId = 'fdm7'
+// Axios.delete(`http://localhost:3001/api/delete/application/${fdmId}`).then((response) => {
+//   console.log(response)
+// });
+
+// let fdmId = 'fdm7'
+// Axios.get(`http://localhost:3001/api/get/applicants`).then((response) => {
+//   console.log(response)
+// });
+
+// Axios.put('http://localhost:3001/api/update/mentee/mentor-id', {
+//   mentorId: "fdm2",
+//   fdmEmail: "fdm0007@fdm.co.uk"
+// }).then((response) => {
+//   console.log(response)
+// });
